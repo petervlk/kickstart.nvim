188610257cd834c25891b5ceebe3e0866549c37b
Peter Vlk <petervlk86@gmail.com>
2025-01-09 09:39:51 +0100

    add keymaps for quickfixList navigation

--- init.lua
+++ init.lua
@@ -165,6 +165,8 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>[', '<cmd>cnext<CR>', { desc = 'Jump to next quickfix item' })
+vim.keymap.set('n', '<leader>]', '<cmd>cprev<CR>', { desc = 'Jump to previous quickfix item' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
