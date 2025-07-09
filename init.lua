e53730a57eca22ffd81eb060a7bde48d193b8ee0
Peter Vlk <petervlk86@gmail.com>
2025-07-09 20:30:54 +0200

    set line width guide

--- init.lua
+++ init.lua
@@ -118,6 +118,8 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.opt.colorcolumn = '100' -- set width guide column at 100 characters
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
