d6ac51d01f122a0240b5f6e2b1637c2b08be5fa3
Peter Vlk <petervlk86@gmail.com>
2025-08-01 14:36:12 +0200

    add project root patterns

--- lua/plugins/project-nvim.lua
+++ lua/plugins/project-nvim.lua
@@ -2,6 +2,18 @@ return {
   'ahmedkhalf/project.nvim',
   config = function()
     require('project_nvim').setup {
+      patterns = {
+        -- git
+        '.git',
+        -- clojure & babashka
+        'deps.edn',
+        'project.edn',
+        'bb.edn',
+        -- java
+        'pom.xml',
+        -- go
+        'go.mod',
+      },
       silent_chdir = false,
     }
   end,
