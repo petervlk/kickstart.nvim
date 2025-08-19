dafb4283be59ebbe7e2128bb4bd1164d2372bbbf
Peter Vlk <petervlk86@gmail.com>
2025-08-19 11:35:00 +0200

    add parinfer-rust plugin

--- init.lua
+++ init.lua
@@ -143,6 +143,19 @@ require('lazy').setup {
     },
 
     {
+      'eraserhd/parinfer-rust',
+      build = 'cargo build --release',
+      init = function()
+        vim.g.parinfer_rust_enabled = 1
+        vim.g.parinfer_rust_mode = 'smart' -- Options: "indent", "paren", "smart"
+        vim.g.parinfer_rust_dim_parens = 1 -- Dim parens in Smart mode
+
+        -- Optional: Set filetypes where parinfer should be active
+        vim.g.parinfer_rust_filetypes = { 'clojure', 'lisp', 'scheme', 'racket' }
+      end,
+    },
+
+    {
       'Olical/conjure',
       ft = { 'clojure', 'fennel' }, -- etc
       lazy = true,
