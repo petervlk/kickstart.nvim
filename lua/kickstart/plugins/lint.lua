e082b9e8e8297e5402336d587376b60543c6f702
Peter Vlk <petervlk86@gmail.com>
2025-08-22 12:13:26 +0200

    change lint config

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -5,10 +5,9 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
-      lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-        clojure = { 'clj-kondo' },
-      }
+      lint.linters_by_ft = lint.linters_by_ft or {}
+      lint.linters_by_ft['clojure'] = { 'clj-kondo' }
+      lint.linters_by_ft['markdown'] = nil
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
